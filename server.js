322a15ae8f8a62e8aaf3f524d9d02bcd67aafd76
KitsadaDev <canc9753@gmail.com>
2024-11-18 04:53:30 +0700

    project-Food

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+const socketIo = require('socket.io')
+const cors = require('cors')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(cors())
+
+const server = app.listen(3000, () => {
+    console.log('serveer is running....')
+})
+
+const io = socketIo(server)
+io.on('connection', (socket) => {
+    console.log('clieent socket connection')
+})
